5e9a1594e13be97f6bd54c4b0ea932281d0c626e
Netta Avidan <nettavidan19@gmail.com>
2021-05-04 13:10:30 +0300

    students complete

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,4 +1,4 @@
-const jwt = require('jsonwebtoken') 
+//const jwt = require('jsonwebtoken') 
 const Users = require('../models/users.models')
 
 const getUsers = async (req,res) =>{
@@ -8,22 +8,6 @@ const getUsers = async (req,res) =>{
 }
 
 const addUser = async (req,res) =>{
-    // const {id, name, age, password, email}  = req.body
-    
-    //     const newUser = new Users({
-    //         id : id, 
-    //         name: name, 
-    //         age: age,
-    //         password: password,
-    //         email: email
-    //     })
-        
-    //     console.log(newUser)
-    //     newUser.save((err) => {
-    //         if (err) return res.status(400).send({"error": err})
-    //         return res.status(200).send({"success": newUser})
-    //     });
-
         const student = new Users(req.body)
         try{
             await student.save()
@@ -36,16 +20,16 @@ const addUser = async (req,res) =>{
 
 const updateMeStudent = async (req,res) =>{
     const updates = Object.keys(req.body)
-    const allowedUpdate = ["id", "name", "age", "email","password"]
+    const allowedUpdate = ["id", "name", "age", "email","password", "subjects"]
     const isValidOperation = updates.every((update) => allowedUpdate.includes(update))
-
     if(!isValidOperation) {
         return res.status(400).send({error: 'Updates most only be regarding credit amount'})
     }
     try{
-        updates.forEach((update) => req.user[update] = req.body[update])
-        await req.user.save()
-        res.send(req.user)
+        updates.forEach((update) => req.student[update] = req.body[update])
+        console.log("netta")
+        await req.student.save()
+        res.send(req.student)
     }
     catch(e){
         res.status(500).send(e)
